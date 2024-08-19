f7f09c787403bd17fd8bda04fee8ab2c4fe35890
slv <slv.4.dev@gmail.com>
2024-08-20 00:22:51 +0300

    Initial Commit

---
+++ eShop (FireBase)/eShop__FireBase_App.swift
@@ -0,0 +1,17 @@
+//
+//  eShop__FireBase_App.swift
+//  eShop (FireBase)
+//
+//  Created by SLV on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct eShop__FireBase_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
